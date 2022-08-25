7d3c16c94c2f2ab508d23654522159adc4606d37
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-25 15:06:52 -0400

    Add work throughs for functions

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
 // functions.js
-function addTwoNumbers() {
-}
+function addTwoNumbers(num1,num2) {
+  return num1 + num2;
+};
+
+function sayHello(name) {
+  var result =null ;
+if (name === 'Will'){
+  result = `No more testing Will!`
+} else {
+result = `Hi there ${name}!`
+};
+return result
+};
+
+function buildCar(color,type){
+var car = {};
+car.color = color;
+car.type = type;
+return car;
+};
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
